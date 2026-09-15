ab0b8f7e8660d4d34f7d1671c17585c34c4167cb
Sulamita Althaus <sulamitaalthaus44@gmail.com>
2026-09-14 21:00:14 -0300

    fix(partners): correct FIAP casing

--- frontend/src/data/partners.ts
+++ frontend/src/data/partners.ts
@@ -1,29 +1,29 @@
 export interface Partner {
-  id: string
-  name: string
-  logoUrl: string
-  websiteUrl: string
-  invertColors?: boolean
+    id: string
+    name: string
+    logoUrl: string
+    websiteUrl: string
+    invertColors?: boolean
 }
 
 export const partnersData: Partner[] = [
-  {
-    id: 'tech-floripa',
-    name: 'Tech Floripa',
-    logoUrl: '/assets/partners/techfloripa.png',
-    websiteUrl: 'https://tech.floripa.br',
-    invertColors: true
-  },
-  {
-    id: 'hostgator',
-    name: 'HostGator',
-    logoUrl: '/assets/partners/hostgator.svg',
-    websiteUrl: 'https://www.hostgator.com.br',
-  },
-  {
-    id: 'fiap',
-    name: 'Fiap',
-    logoUrl: '/assets/partners/fiap.svg',
-    websiteUrl: 'https://www.fiap.com.br',
-  },
+    {
+        id: 'tech-floripa',
+        name: 'Tech Floripa',
+        logoUrl: '/assets/partners/techfloripa.png',
+        websiteUrl: 'https://tech.floripa.br',
+        invertColors: true
+    },
+    {
+        id: 'hostgator',
+        name: 'HostGator',
+        logoUrl: '/assets/partners/hostgator.svg',
+        websiteUrl: 'https://www.hostgator.com.br',
+    },
+    {
+        id: 'fiap',
+        name: 'FIAP',
+        logoUrl: '/assets/partners/fiap.svg',
+        websiteUrl: 'https://www.fiap.com.br',
+    },
 ]
